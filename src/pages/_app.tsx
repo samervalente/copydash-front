56c8c9fc48791a34646d2f7275d40d1069180981
Samer Valente <samervalente@gmail.com>
2024-01-06 19:32:23 -0300

    add initial configs and theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,38 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from 'next/head';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import { createTheme } from '../theme';
+import 'simplebar-react/dist/simplebar.min.css';
+import { NextPage } from 'next';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = (props: { Component: any; pageProps: any; }) => {
+  const { Component, pageProps } = props;
+
+
+  const getLayout = Component.getLayout ?? ((page: NextPage) => page);
+
+  const theme = createTheme();
+
+  return (
+
+    <>
+      <Head>
+        <title>
+          CopyDash
+        </title>
+        <meta
+          name="viewport"
+          content="initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {getLayout(<Component {...pageProps} />)}
+      </ThemeProvider>
+    </>
+
+  );
+};
+
+export default App;
